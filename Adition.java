be129b70972a62f992ca57e001e767efe5aa008e
unknown <mukambetova_n@12-02.iuca.loc>
2016-09-15 11:47:00 +0600

    j

---
+++ Adition.java
@@ -0,0 +1,14 @@
+class Adition
+ 
+{ 
+   public static void main ( String[] args )
+   { 
+	System.out.print("vvedite chislo");
+	
+	int a = 5;
+	int b = 7;
+	int c = a + b;
+	System.out.print("value of c:");
+	System.out.println( c);
+   }
+} 
